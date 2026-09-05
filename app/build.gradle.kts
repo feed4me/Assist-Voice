6d06cea06630539ac20bdbfb1f015a5ab682ac8f
feed4me <feed4me@yandex.com>
2026-09-05 20:03:30 +0000

    bump to v1.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.nikolay.assistvoice"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.11"
+        versionCode = 3
+        versionName = "1.12"
 
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a")
